74a80d9557269d7068999d1240a0d018b647ded5
ameer017 <rajiabdullahi907@gmail.com>
2025-01-17 19:43:57 +0100

    Fix next-pwa issue

--- pwa-app/next.config.ts
+++ pwa-app/next.config.ts
@@ -3,10 +3,14 @@ import type { NextConfig } from "next";
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+
 });
 
 const nextConfig: NextConfig = withPWA({
   /* config options here */
+  dest: "public",
+  register: true,
+  skipWaiting: true,
 });
 
 export default nextConfig;
