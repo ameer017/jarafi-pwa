3bb4442fce49de5182497c60d1ab44af022f20eb
ameer017 <rajiabdullahi907@gmail.com>
2025-01-19 21:54:42 +0100

    Onboarding screens structured

--- jara-client/src/pages/onboarding/GetStarted.jsx
+++ jara-client/src/pages/onboarding/GetStarted.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default GetStarted
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/main-screen");
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+  return (
+    <div className="bg-[#0F0140] h-screen w-full p-4 flex items-center justify-center">
+      <img
+        src="/get-started.png"
+        alt="Get Started"
+        className="w-full max-w-md h-auto object-contain"
+      />
+    </div>
+  );
+};
+
+export default GetStarted;
