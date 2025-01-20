1b23fd1371d7a69080c3f9f30b321991f97b8e04
ameer017 <rajiabdullahi907@gmail.com>
2025-01-20 07:32:48 +0100

    onboarding-page setup

--- jara-client/src/pages/onboarding/TWE.jsx
+++ jara-client/src/pages/onboarding/TWE.jsx
@@ -11,20 +11,32 @@ const TWE = () => {
   return (
     <TransitionGroup component={null}>
       <CSSTransition key={location.key} classNames="slide" timeout={500}>
-        <div className="bg-[#0F0140] h-screen w-full p-6 flex flex-col items-center justify-center gap-[80px] text-center text-white">
-          {/* Background Images */}
-          <div className="absolute top-0 left-0">
-            <img src="/bg-up.png" alt="Background Top Left" />
-          </div>
-          <div className="absolute bottom-0 left-0">
-            <img src="/bg-down.svg" alt="Background Bottom Left" />
-          </div>
-          <div className="absolute bottom-0 right-0 rotate-180">
-            <img src="/bg-up.png" alt="Background Bottom Right" />
-          </div>
-          <div className="absolute top-0 right-0 rotate-180">
-            <img src="/bg-down.svg" alt="Background Top Right" />
-          </div>
+        <div className="bg-[#0F0140] h-screen w-full p-6 flex flex-col items-center justify-center gap-[80px] text-center text-white relative">
+          <img
+            src="/bg-up.png"
+            alt="Background Top Left"
+            className="absolute top-0 left-0 w-36 md:w-48 lg:w-64"
+          />
+          <img
+            src="/bg-down.svg"
+            alt="Background Bottom Left"
+            className="absolute bottom-10 left-0 w-28 md:w-40 lg:w-56"
+          />
+          <img
+            src="/bgSeven.png"
+            alt="Background Center Right"
+            className="absolute top-52 right-16 w-40 md:w-56 lg:w-72 hidden md:block"
+          />
+          <img
+            src="/bgTen.png"
+            alt="Background Top Right"
+            className="absolute top-0 right-0 w-36 md:w-48 lg:w-64"
+          />
+          <img
+            src="/bgTen.png"
+            alt="Background Bottom Right Rotated"
+            className="absolute md:-bottom-9 md:right-0 -bottom-6 right-6 w-36 md:w-48 lg:w-64 rotate-90"
+          />
 
           <div className="flex flex-col items-center space-y-6 mt-12">
             <img
@@ -35,16 +47,14 @@ const TWE = () => {
 
             <h1 className="text-3xl md:text-4xl font-bold">Trade with Ease</h1>
 
-            <p className="text-lg md:text-xl max-w-lg leading-relaxed">
+            <p className="text-lg md:text-xl max-w-lg leading-relaxed w-[350px] ">
               Trade your stable coins to fiat currency using our trusted P2P
               agents!
             </p>
           </div>
 
-          {/* Navigation Section */}
           <div className="mb-12 space-y-8 flex flex-col items-center gap-[80px]">
             <div className="flex space-x-4">
-              {/* Navigation Buttons */}
               <button
                 onClick={() => navigate("/main-screen")}
                 className={`border-[1.2px] p-[1.8px] rounded-lg text-md font-medium shadow hover:bg-gray-200 duration-300 ${
@@ -73,7 +83,6 @@ const TWE = () => {
               ></button>
             </div>
 
-            {/* Action Button */}
             <button
               className="bg-[#F2E205] py-3 px-8 text-[#4F4E50] rounded-lg text-lg font-medium shadow-lg w-[280px] md:w-[350px] hover:bg-[#e4d704] transition-transform transform scale-90 hover:scale-100"
               onClick={() => navigate("/sign-up-endpoint")}
