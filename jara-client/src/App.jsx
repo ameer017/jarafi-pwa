c61c241741e596ab8454d92058b5c7a8eff5bca0
MatexConcept <matexconcept@gmail.com>
2025-01-23 18:34:01 +0100

    recieve-page

--- jara-client/src/App.jsx
+++ jara-client/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Recieve from './component/Transactions/Receive'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/recieve" element={<Recieve/>} />
+
       </Routes>
     </>
   );
