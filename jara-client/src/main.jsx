c0d043502e851d8f6a45d7be3b961a646da517eb
ameer017 <rajiabdullahi907@gmail.com>
2025-03-17 21:53:07 +0100

    app configured and running

--- jara-client/src/main.jsx
+++ jara-client/src/main.jsx
@@ -12,6 +12,22 @@ import "@getpara/react-sdk/styles.css";
 
 const queryClient = new QueryClient();
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      })
+      .catch((err) => {
+        console.log("ServiceWorker registration failed: ", err);
+      });
+  });
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <WagmiProvider config={config}>
