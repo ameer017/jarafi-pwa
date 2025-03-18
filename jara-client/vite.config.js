bde24e261bf35e575315d209e437da2ef4cf0c31
ameer017 <rajiabdullahi907@gmail.com>
2025-03-18 22:01:56 +0100

    bundled and built ready for play store

--- jara-client/vite.config.js
+++ jara-client/vite.config.js
@@ -15,30 +15,7 @@ export default defineConfig({
         "robots.txt",
         "apple-touch-icon.png",
       ],
-      manifest: {
-        name: "Jarafi PWA",
-        short_name: "Jarafi",
-        description: "Building an Inclusive Onchain Finance for Everyone",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "/icon512_maskable.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icon512_rounded.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/icon512_maskable.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
+      manifest: "/manifest.json",
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg,json}"], // Cache all necessary file types
         runtimeCaching: [
